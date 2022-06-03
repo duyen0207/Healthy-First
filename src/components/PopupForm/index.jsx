d886da18a39505d594a34021e1458700d48ec114
Nugvenus2010 <ngocnhung2k1@gmail.com>
2022-06-04 00:14:22 +0700

    feat: add popup form

---
+++ src/components/PopupForm/index.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Modal, Button, Input } from "antd";
+import style from "./PopupForm.module.scss";
+
+function Popup({
+  isVisible,
+  title,
+  okButton,
+  handleCancel,
+  inputList,
+  handleSubmit,
+}) {
+  const [input, setInput] = useState({});
+  return (
+    <Modal
+      visible={isVisible}
+      title={title}
+      onCancel={handleCancel}
+      footer={[
+        <Button
+          key="back"
+          onClick={handleCancel}
+          style={{
+            color: "#30ABC2",
+            borderRadius: "10px",
+            borderColor: "#30ABC2",
+          }}
+        >
+          Hủy
+        </Button>,
+        <Button
+          key="submit"
+          type="primary"
+          onClick={() => {
+            handleSubmit(input);
+          }}
+          style={{ backgroundColor: "#30ABC2", borderRadius: "10px" }}
+        >
+          {okButton}
+        </Button>,
+      ]}
+    >
+      {inputList.map((input) => (
+        <div key={input.name}>
+          <label>{input.label}</label>
+          <Input
+            name={input.name}
+            required
+            className={style.input}
+            type="text"
+            placeholder={input}
+            onChange={(e) => {
+              const temp = {};
+              temp[input.name] = e.target.value;
+              setInput((previous) => {
+                return { ...previous, ...temp };
+              });
+            }}
+          />
+        </div>
+      ))}
+    </Modal>
+  );
+}
+
+export default Popup;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,42 @@
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+import PopupForm from "../../components/PopupForm";
+
 function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+  const handleShowPopup = () => {
+    setShowPopup(true);
+  };
+  const handleCancel = () => {
+    setShowPopup(false);
+  };
+
+  //TODO: sửa hàm handleSubmit để dùng api (ví dụ như tạo mới hoặc cập nhật cơ sở kinh doanh)
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
   return (
-    <div>
-      {/* <h1>Home</h1> */}
-    
-    </div>
+    <>
+      <Button type="primary" onClick={handleShowPopup}>
+        Show popup
+      </Button>
+      {showPopup && (
+        <PopupForm
+          isVisible={showPopup}
+          title={"Sửa cơ sở kinh doanh"}
+          okButton={"Cập nhật"}
+          handleCancel={handleCancel}
+          inputList={[
+            { label: "Tên cơ sở", name: "storeName" },
+            { label: "Địa chỉ", name: "address" },
+            { label: "Sđt", name: "phoneNumber" },
+            { label: "Loại hình kinh doanh", name: "typeOfBusiness" },
+          ]}
+          handleSubmit={handleSubmit}
+        />
+      )}
+    </>
   );
 }
 
